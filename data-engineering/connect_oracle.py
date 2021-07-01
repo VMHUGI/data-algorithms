8dfc4657a26e5707e7c41e655a063ccc55c7eb81
Nhalim Yuri <nhalim.uni@gmail.com>
2021-07-01 14:59:29 -0500

    Update connect_oracle.py

--- data-engineering/connect_oracle.py
+++ data-engineering/connect_oracle.py
@@ -6,15 +6,15 @@ from pyspark.sql import SparkSession
 def iniciaClienteOracle(rutaOracleCliente):
     cx_Oracle.init_oracle_client(lib_dir = rutaOracleCliente)
 
-def engine_general(USUARIO, PASSWORD, IP, PUERTO, DDL):
-    return  create_engine('oracle://' + USUARIO + ':' + PASSWORD + '@' + IP + ':' + PUERTO + '/' + DDL, echo=False)
+def engine_general(USUARIO, PASSWORD, IP, PUERTO, SID):
+    return  create_engine('oracle://' + USUARIO + ':' + PASSWORD + '@' + IP + ':' + PUERTO + '/' + SID, echo=False)
 
-def engine_spark(USUARIO, PASSWORD, IP, PUERTO, DDL):
+def engine_spark(USUARIO, PASSWORD, IP, PUERTO, SID):
     engine = {
         "user": USUARIO, 
         "password": PASSWORD, 
         "driver": "oracle.jdbc.driver.OracleDriver"
     }
-    url = "jdbc:oracle:thin:@//"+IP+":"+PUERTO+"/"+"DDL"
+    url = "jdbc:oracle:thin:@//"+IP+":"+PUERTO+"/"+SID
     return engine,url
     #AFTER this, call like this df_csv.write.jdbc(url=url, table=table, mode=mode, properties=engine)
